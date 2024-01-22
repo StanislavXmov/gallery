469f7170aeef85b07f20b57d700dcc23f5e63895
stanislav <xiiiovart@gmail.com>
2024-01-22 20:42:21 +0300

    add scroll logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Works from './components/Works';
 import Footer from './components/Footer';
 
 import './App.css';
+import SvgLogo from './components/SvgLogo';
 
 function App() {
 
   return (
     <>
+      <SvgLogo />
       <Header />
       <About />
       <Works />
